b0d3ac6da50a07d91180630f77d19a3b2b1df73d
tatsuya-asami <sharekobekko@gmail.com>
2024-08-10 21:14:06 +0900

    chore: Update npm dependencies and remove unused test files

--- src/content/Content.tsx
+++ src/content/Content.tsx
@@ -1,12 +1,47 @@
-import { Counter } from '../app/features/counter';
+import { useCallback, useEffect } from 'react';
+import { useBucket } from '@extend-chrome/storage';
 
 const Content = () => {
-  return (
-    <div className="fixed z-[999] bottom-2 right-2 shadow-xl border-[1px] bg-white bg-opacity-10">
-      <div className="flex justify-center mt-2 text-base">Content Counter</div>
-      <Counter />
-    </div>
-  );
+  const INDEXED_DB_NAME = 'firebaseLocalStorageDb';
+  const STORAGE_NAME = 'firebaseLocalStorage';
+  const bucket = useBucket<{ token: string }>('local', 'token');
+
+  const getToken = useCallback(() => {
+    const request = window.indexedDB.open(INDEXED_DB_NAME);
+
+    request.onsuccess = (event) => {
+      // @ts-expect-error target is not defined
+      const db = event.target?.result as IDBDatabase;
+      console.log(db.objectStoreNames);
+
+      const transaction = db.transaction([STORAGE_NAME], 'readonly');
+      const objectStore = transaction.objectStore(STORAGE_NAME);
+
+      objectStore.getAll().onsuccess = async (event) => {
+        // @ts-expect-error target is not defined
+        const result = event.target?.result;
+        const value = result[0].value;
+        const accessToken = value.stsTokenManager.accessToken;
+        bucket.set({ token: accessToken });
+        console.log(accessToken);
+        console.log(await bucket.get());
+      };
+
+      objectStore.getAll().onerror = function (event) {
+        console.error('Error fetching data from firebaseLocalStorage:', event);
+      };
+    };
+
+    request.onerror = function (event) {
+      console.error('Error opening firebaseLocalStorageDb:', event);
+    };
+  }, [bucket]);
+
+  useEffect(() => {
+    getToken();
+  }, [getToken]);
+
+  return null;
 };
 
 export default Content;
