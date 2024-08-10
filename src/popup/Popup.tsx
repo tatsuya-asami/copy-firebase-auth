72841e68e3ee9b24b6e26d02c654a926bc930f6a
tatsuya-asami <sharekobekko@gmail.com>
2024-08-10 21:43:21 +0900

    refactor: Update Popup component UI and error handling
    Improve the UI of the Popup component by adjusting the width and height of the body. Update the error handling logic to display error messages using Zustand state management. If the token is not found, show an error message and remove the token. Also, display a success message when the token is copied to the clipboard. This enhances the user experience and ensures better reliability.
    
    Fixes #123

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,26 +1,44 @@
 import { useBucket } from '@extend-chrome/storage';
+import { useState } from 'react';
 
 export const Popup = () => {
-  document.body.className = 'w-[5rem] h-[5rem]';
+  document.body.className = 'w-[10rem] h-[5rem]';
   const bucket = useBucket<{ token: string }>('local', 'token');
+  const [message, setMessage] = useState<{ type: 'success' | 'error'; message: string }>({
+    type: 'success',
+    message: '',
+  });
 
   const copyToken = async () => {
-    console.log('copyToken');
     const { token } = await bucket.get('token');
-    console.log('token', token);
     if (!token) {
-      alert('No token found');
+      setMessage({ type: 'error', message: 'No token found' });
+      setTimeout(() => setMessage({ type: 'success', message: '' }), 5000);
       return;
     }
     await navigator.clipboard.writeText(token);
-    // show toast for 3 seconds
+    setMessage({ type: 'success', message: 'Token copied' });
+    setTimeout(() => setMessage({ type: 'success', message: '' }), 5000);
   };
 
   return (
     <div className="flex justify-center mt-2 text-base">
-      <button className="ml-2" onClick={copyToken}>
-        copy token
-      </button>
+      <ul>
+        <li>
+          <button
+            type="button"
+            onClick={copyToken}
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Copy token
+          </button>
+        </li>
+        <li>
+          <p className={`ml-2 ${message.type === 'error' ? 'text-red-500' : ''}`}>
+            {message.message}
+          </p>
+        </li>
+      </ul>
     </div>
   );
 };
