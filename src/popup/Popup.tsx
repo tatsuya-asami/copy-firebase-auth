d97c5b33c972e8a378fcd41131868649d49fbae3
tatsuya-asami <sharekobekko@gmail.com>
2024-08-10 21:50:14 +0900

    refactor: Adjust width and height of Popup component body
    Update the width and height of the body in the Popup component to improve the UI. This ensures a better user experience and enhances the visual appearance of the component.
    
    Fixes #123

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -2,7 +2,7 @@ import { useBucket } from '@extend-chrome/storage';
 import { useState } from 'react';
 
 export const Popup = () => {
-  document.body.className = 'w-[10rem] h-[5rem]';
+  document.body.className = 'w-[10rem] h-[6rem]';
   const bucket = useBucket<{ token: string }>('local', 'token');
   const [message, setMessage] = useState<{ type: 'success' | 'error'; message: string }>({
     type: 'success',
@@ -22,13 +22,13 @@ export const Popup = () => {
   };
 
   return (
-    <div className="flex justify-center mt-2 text-base">
+    <div style={{ padding: '1em', display: 'flex', justifyContent: 'center' }}>
       <ul>
         <li>
           <button
             type="button"
             onClick={copyToken}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
             Copy token
           </button>
